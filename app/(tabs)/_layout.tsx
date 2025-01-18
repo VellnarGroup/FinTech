94e7a424e6fa1253332399248822419e785d3fc5
Vellnar <194289062+VellnarGroup@users.noreply.github.com>
2025-01-18 19:29:02 +0100

    layout delle tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,50 +1,80 @@
-import React from 'react'
-import FontAwesome from '@expo/vector-icons/FontAwesome'
-import { Link, Tabs } from 'expo-router'
-import { Pressable } from 'react-native'
+import Colors from '@/constants/Colors';
+import { FontAwesome } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import { BlurView } from 'expo-blur';
+import CustomHeader from '@/components/CustomHeader';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
-export default function TabLayout() {
-
+const Layout = () => {
   return (
     <Tabs
       screenOptions={{
-        headerShown: true,
+        tabBarActiveTintColor: Colors.primary,
+        tabBarBackground: () => (
+          <BlurView
+            intensity={100}
+            tint={'extraLight'}
+            style={{
+              flex: 1,
+              backgroundColor: 'rgba(0,0,0,0.05)',
+            }}
+          />
+        ),
+        tabBarStyle: {
+          backgroundColor: 'transparent',
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          borderTopWidth: 0,
+        },
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          title: 'Home',
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="registered" size={size} color={color} />
+          ),
+          header: () => <CustomHeader />,
+          headerTransparent: true,
+        }}
+      />
+      <Tabs.Screen
+        name="invest"
+        options={{
+          title: 'Invest',
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="line-chart" size={size} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="transfers"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Transfers',
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="exchange" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="crypto"
+        options={{
+          title: 'Crypto',
+          tabBarIcon: ({ size, color }) => <FontAwesome name="bitcoin" size={size} color={color} />,
+          header: () => <CustomHeader />,
+          headerTransparent: true,
+        }}
+      />
+      <Tabs.Screen
+        name="lifestyle"
+        options={{
+          title: 'Lifestyle',
+          tabBarIcon: ({ size, color }) => <FontAwesome name="th" size={size} color={color} />,
         }}
       />
     </Tabs>
   );
-}
+};
+export default Layout;
